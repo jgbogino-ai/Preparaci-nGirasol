1ea9b6dc946080075462fdcb1d23efdf4c45186a
jgbogino-ai <jgbogino@gmail.com>
2026-07-23 10:14:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -369,13 +369,11 @@ if gases > 80:
 # ======================================
 
 st.subheader("📋 Últimos Registros")
-st.write(df.dtypes)
+
 df_mostrar = df.copy()
 
-for col in df_mostrar.columns:
-    df_mostrar[col] = df_mostrar[col].astype(str)
 
 st.dataframe(
-    df_mostrar.tail(20),
+    df.tail(20).astype(str)
     use_container_width=True
 )
