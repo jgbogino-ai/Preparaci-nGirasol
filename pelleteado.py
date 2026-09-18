621d7d8170eaeed5e8ee2803d9e5db627bb94474
jgbogino-ai <jgbogino@gmail.com>
2026-09-18 15:15:49 -0300

    Update pelleteado.py

--- pelleteado.py
+++ pelleteado.py
@@ -45,6 +45,28 @@ if df.empty:
 
 ultimo = df.iloc[-1]
 
+
+# ==================================================
+# ENCABEZADO
+# ==================================================
+
+col1, col2 = st.columns([1, 4])
+
+with col1:
+    st.image(
+        "logo glp.png",
+        width=180
+    )
+
+with col2:
+    st.markdown(
+        """
+        <h1 style="margin-top:15px;">
+        🏭 Pelleteado
+        </h1>
+        """,
+        unsafe_allow_html=True
+    )
 # ==================================================
 # ULTIMA CARGA
 # ==================================================
